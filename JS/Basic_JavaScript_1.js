54918e55bb256b526031befc48f365c9824bd4a0
S-DeLeonardo <Sdeleonardo@gmail.com>
2020-05-04 00:49:18 +0900

    Initial commit

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,32 @@
+window.alert("Hello,World!");
+
+document.write("Hello, again World!");
+
+var X = "Hello,World!";
+
+document.write(X);
+
+var A = "An example of a string of text.";
+
+document.write(A);
+
+var S = "Please confirm you want to enter this site.";
+
+window.alert(S);
+
+var escape = "\"This is an example of an escape\"";
+
+document.write(escape);
+
+document.write("\"This is an example of concatenating.\"" + "concatenating is strange");
+
+var C = "Concatenated string" + "example";
+
+document.write(C);
+
+var animal = "Cats", catColor1 = "black", catColor2 = "white", catColor3 = "grey";
+var animal = animal.fontcolor("red"), catColor1= catColor1.fontcolor("black"), catColor2= catColor2.fontcolor("blue"), catColor3 = catColor3.fontcolor("grey")
+
+document.write(animal + catColor1, animal + catColor2, animal + catColor3)
+
+document.write(4+4);
