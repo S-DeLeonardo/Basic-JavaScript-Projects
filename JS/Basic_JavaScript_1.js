9bbbd3db223974f08bad17c4b61376e0f0a10020
S-DeLeonardo <Sdeleonardo@gmail.com>
2020-05-20 14:05:48 +0900

    Upload Basic_HTML_2 and Basic_JavaScript_2
    Uploaded two new files required for an assignment. Minor changes to Basic_HTML_1 and Basic_JavaScript_1.

--- JS/Basic_JavaScript_1.js
+++ JS/Basic_JavaScript_1.js
@@ -1,10 +1,10 @@
-window.alert("Hello,World!");
+window.alert("Hello,World!"); //Opens an alert box that says "Hello,WOrld!"
 
-document.write("Hello, again World!");
+document.write("Hello, again World!"); // Writes the sentence on the webpage.
 
-var X = "Hello,World!";
+var X = "Hello,World!"; // Sets the variable x to the text "Hello,World!"
 
-document.write(X);
+document.write(X); //Writes out the text we set as variable x
 
 var A = "An example of a string of text.";
 
@@ -12,21 +12,21 @@ document.write(A);
 
 var S = "Please confirm you want to enter this site.";
 
-window.alert(S);
+window.alert(S); //Opens an alert box with the value set as variable S
 
-var escape = "\"This is an example of an escape\"";
+var escape = "\"This is an example of an escape\""; //An escape is used to display things like quotation marks that are used in coding
 
 document.write(escape);
 
-document.write("\"This is an example of concatenating.\"" + "concatenating is strange");
+document.write("\"This is an example of concatenating.\"" + "concatenating is strange"); //Concatenating is used to link things
 
 var C = "Concatenated string" + "example";
 
-document.write(C);
+document.write(C); //Concatenating using a variable
 
 var animal = "Cats", catColor1 = "black", catColor2 = "white", catColor3 = "grey";
-var animal = animal.fontcolor("red"), catColor1= catColor1.fontcolor("black"), catColor2= catColor2.fontcolor("blue"), catColor3 = catColor3.fontcolor("grey")
+var animal = animal.fontcolor("red"), catColor1= catColor1.fontcolor("black"), catColor2= catColor2.fontcolor("blue"), catColor3 = catColor3.fontcolor("grey") //Setting a fontcolor to a variable
 
 document.write(animal + catColor1, animal + catColor2, animal + catColor3)
 
-document.write(4+4);
+document.write(4+4); // an expression using two values and an operator
---
+++ JS/Basic_JavaScript_2.js
@@ -0,0 +1,19 @@
+function My_First_Function () { //Defining the function and giving it a name
+    
+    var str = "This text is blue!"; //Defining a variable and assinging it a string value
+
+    var result = str.fontcolor("blue"); //Using the fontcolor method on the str variable
+
+    document.getElementById("Blue_Text").innerHTML = result; //Putting the value of result into HTML elemet with "Blue_Text" id
+
+}
+
+function Concatenate_Example () {
+    var Concatenate_text = "The text should turn red,";
+
+    Concatenate_text += " did it work?";
+
+    var color = Concatenate_text.fontcolor("red");
+    
+    document.getElementById("Concatenate").innerHTML = color;
+}
